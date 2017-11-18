50900127f2746e72bf426f117affb125a4c0171e
Nicholas Aelick <n.aelick@gmail.com>
2017-11-18 01:09:52 -0500

    Add spacestation image, make physics optional for game state update

--- src/SpaceGame/Systems/game_state_request.py
+++ src/SpaceGame/Systems/game_state_request.py
@@ -13,28 +13,42 @@ class GameStateRequestSystem(System):
         print(pnode.player_input.data[-1])
 
         nodes = self.node_factory.create_node_list(
-            ["position", "velocity", "mass", "acceleration", "force", "rotation", "type", "physics_update"], ["player_input"])
+            ["position", "type"], ["velocity", "mass", "acceleration", "force", "rotation", "physics_update", "player_input"])
 
         for node in nodes:
+
+            velx = node.velocity.x if node.has('velocity') else 0
+            vely = node.velocity.y if node.has('velocity') else 0
+            mass = node.mass.mass if node.has('mass') else 1
+            accelx = node.acceleration.x if node.has('acceleration') else 0
+            accely = node.acceleration.y if node.has('acceleration') else 0
+            forcex = node.force.x if node.has('force') else 0
+            forcey = node.force.y if node.has('force') else 0
+            rotation = node.rotation.rotation if node.has('rotation') else 0
+            control = node.player_input.data[
+                -1] if node.has('player_input') else None,
+            last_update = node.physics_update.last_update if node.has(
+                physics_update) else 0
+
             game_state["entities"][node.id] = {
                 "id": node.id,
                 "position": {"x": node.position.x,
                              "y": node.position.y
                              },
-                "velocity": {"x": node.velocity.x,
-                             "y": node.velocity.y
+                "velocity": {"x": velx,
+                             "y": vely
                              },
-                "acceleration": {"x": node.acceleration.x,
-                                 "y": node.acceleration.y
+                "acceleration": {"x": accelx,
+                                 "y": accely
                                  },
-                "force": {"x": node.force.x,
-                          "y": node.force.y
+                "force": {"x": forcex,
+                          "y": forcey
                           },
-                "mass": node.mass.mass,
+                "mass": mass,
                 "type": node.type.type,
-                "rotation": node.rotation.rotation,
-                "control": node.player_input.data[-1] if node.has('player_input') else None,
-                "last_update": node.physics_update.last_update
+                "rotation": rotation,
+                "control": control,
+                "last_update": last_update
             }
         # print("Returning game state request")
 
