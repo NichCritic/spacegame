5c96aae2ead45218268d1be616e5ace30b0c9d1b
Nicholas Aelick <naelick@espial.com>
2020-01-09 22:27:53 -0500

    Bunch of UI improvements

--- src/SpaceGame/static/js/CameraFollowSystem.js
+++ src/SpaceGame/static/js/CameraFollowSystem.js
@@ -24,16 +24,19 @@ var CameraFollowSystem = (function() {
 		let camera_offset_x = brake ? 0 : node.velocity.x * 250;
 		let camera_offset_y = brake ? 0 : node.velocity.y * 500;
 
-		if(this.offset_x > 10) {
-			camera_offset_x = Math.max(camera_offset_x, this.offset_x - 10);
-		} else if (this.offset_x < -10) {
-			camera_offset_x = Math.min(camera_offset_x, this.offset_x + 10);
+		x_return_speed = 3
+		y_return_speed = 6
+
+		if(this.offset_x > x_return_speed) {
+			camera_offset_x = Math.max(camera_offset_x, this.offset_x - x_return_speed);
+		} else if (this.offset_x < -x_return_speed) {
+			camera_offset_x = Math.min(camera_offset_x, this.offset_x + x_return_speed);
 		}
 
-		if(this.offset_y > 20) {
-			camera_offset_y = Math.max(camera_offset_y, this.offset_y - 20);
-		} else if (this.offset_y < -20){
-			camera_offset_y = Math.min(camera_offset_y, this.offset_y + 20);
+		if(this.offset_y > y_return_speed) {
+			camera_offset_y = Math.max(camera_offset_y, this.offset_y - y_return_speed);
+		} else if (this.offset_y < -y_return_speed){
+			camera_offset_y = Math.min(camera_offset_y, this.offset_y + y_return_speed);
 		}
 
 		camera.position.x = node.position.x - this.width/2 + camera_offset_x;
