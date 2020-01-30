c560b2b833d67eb2632e1844340823de8ef106e6
Nicholas Aelick <n.aelick@gmail.com>
2020-01-29 19:30:21 -0500

    Fix some typos

--- src/SpaceGame/utils/spatial_map.py
+++ src/SpaceGame/utils/spatial_map.py
@@ -1,61 +1,60 @@
 import math
 from collections import defaultdict
 
+
 class SpatialMap():
-	
-	def __init__(self, size):
-		self.size = size
-		self.map = defaultdict(set)
-		self.entity_map = defaultdict(set)
 
-	def add(id, x, y, radius=0):
-		s_left = math.floor((x - radius) / self.size)
-		s_right = math.floor((x + radius) / self.size)
+    def __init__(self, size):
+        self.size = size
+        self.map = defaultdict(set)
+        self.entity_map = defaultdict(set)
 
-		s_top = math.floor((y - radius) / self.size)
-		s_bottom = math.floor((y + radius) / self.size)
+    def add(self, id, x, y, radius=0):
+        s_left = math.floor((x - radius) / self.size)
+        s_right = math.floor((x + radius) / self.size)
 
-		for j in range(s_top, s_bottom):
-			for i in range(s_left, s_right):
-				self.map[(i, j)].add(id)
-				self.entity_map[id].add((i, j))
+        s_top = math.floor((y - radius) / self.size)
+        s_bottom = math.floor((y + radius) / self.size)
 
-		return self.entity_map[id]
+        for j in range(s_top, s_bottom):
+            for i in range(s_left, s_right):
+                self.map[(i, j)].add(id)
+                self.entity_map[id].add((i, j))
 
-	def move(id, x, y, radius):
-		s_left = math.floor((x - radius) / self.size)
-		s_right = math.floor((x + radius) / self.size)
+        return self.entity_map[id]
 
-		s_top = math.floor((y - radius) / self.size)
-		s_bottom = math.floor((y + radius) / self.size)
+    def move(self, id, x, y, radius):
+        s_left = math.floor((x - radius) / self.size)
+        s_right = math.floor((x + radius) / self.size)
 
-		coords = set([(i, j) for i in range(s_left, s_right) for j in range(s_top, s_bottom)])
+        s_top = math.floor((y - radius) / self.size)
+        s_bottom = math.floor((y + radius) / self.size)
 
-		old_coords = self.entity_map[id] - coords
-		new_coords = coords - self.entity_map[id]
+        coords = set([(i, j) for i in range(s_left, s_right)
+                      for j in range(s_top, s_bottom)])
 
-		self.entity_map[id] = coords
-		for coord in old_coords:
-			self.map[coord].remove(id)
+        old_coords = self.entity_map[id] - coords
+        new_coords = coords - self.entity_map[id]
 
-		for coord in new_coords:
-			self.map[coord].add(id)
-		return coords
+        self.entity_map[id] = coords
+        for coord in old_coords:
+            self.map[coord].remove(id)
 
+        for coord in new_coords:
+            self.map[coord].add(id)
+        return coords
 
-	def remove(id):
-		for coord in self.entity_map[id]:
-			self.map[coord].remove(id)
-		del self.entity_map[id]
+    def remove(self, id):
+        for coord in self.entity_map[id]:
+            self.map[coord].remove(id)
+        del self.entity_map[id]
 
-	def neighbours(id):
-		ns = set()
-		for coord in self.entity_map[id]:
-			ns += self.map[coord]
-		return ns
+    def neighbours(self, id):
+        ns = set()
+        for coord in self.entity_map[id]:
+            ns += self.map[coord]
+        return ns
 
-	def neighbours_coords(x, y):
-		s_x, s_y = math.floor(x / self.size), math.floor(y / self.size)
-		return self.map[(s_x, s_y)]
-
-
+    def neighbours_coords(self, x, y):
+        s_x, s_y = math.floor(x / self.size), math.floor(y / self.size)
+        return self.map[(s_x, s_y)]
